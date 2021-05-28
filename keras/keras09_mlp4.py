5e02872522b922a4d1000cde8c62bd32657843fe
unknown <ahngeo11@gmail.com>
2021-05-28 21:32:37 +0900

    210528

--- keras/keras09_mlp4.py
+++ keras/keras09_mlp4.py
@@ -1,37 +1,28 @@
-#R2 음수 아닌 0.5 이하로
-#레이어는 총 6개 이상
-#batchsize 1, epoch 100이상
-#은닉층 노드 10~1000
-#데이터 조작 금지
-
+#다 : 1 mlp
 import numpy as np
-#1.
-x_train = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-y_train = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-x_test = np.array([11, 12, 13, 14, 15])
-y_test = np.array([11, 12, 13, 14, 15])
-#x_pred = np.array([16, 17, 18])
-
-#2.
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
+x = np.array([range(100), range(301, 401), range(1, 101)])
+y = np.array(range(711, 811))
+
+x = np.transpose(x)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, 
+    train_size=0.8, random_state=66) #랜덤 계수(random_state) = shuffle 순서 고정
+
+#2.
 model = Sequential()
-model.add(Dense(10, input_shape=(1,), activation='relu'))
-model.add(Dense(987))
-model.add(Dense(10))
-model.add(Dense(654))
-model.add(Dense(10))
-model.add(Dense(987))
-model.add(Dense(10))
-model.add(Dense(654))
+model.add(Dense(10, input_shape=(3,), activation='relu'))
+model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
 
 #3.
 model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, epochs=5000, batch_size=1)
+model.fit(x_train, y_train, epochs=60, batch_size=1)
 
 #4.
 loss = model.evaluate(x_test, y_test)
